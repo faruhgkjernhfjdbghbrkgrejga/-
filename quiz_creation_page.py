12763e12f0f6b2759720690866215ac62dac9256
faruhgkjernhfjdbghbrkgrejga <165423455+faruhgkjernhfjdbghbrkgrejga@users.noreply.github.com>
2024-04-27 15:03:58 +0900

    Update quiz_creation_page.py

--- quiz_creation_page.py
+++ quiz_creation_page.py
@@ -1,5 +1,3 @@
-#quiz_creation_page.py
-
 import streamlit as st
 from langchain_openai import ChatOpenAI
 from langchain_core.pydantic_v1 import BaseModel, Field
@@ -161,8 +159,9 @@ def quiz_creation_page():
                 if st.button('문제 생성 하기'):
                     for i in range(num_quizzes):
                         quiz_questions.append(generate_quiz(quiz_type, text_content))
+                        print(quiz_questions)
                         st.session_state['quizs'] = quiz_questions
                     st.session_state.selected_page = "퀴즈 풀이"
                     st.session_state.selected_type = quiz_type
                     st.session_state.selected_num = num_quizzes
-                    st.rerun()
+                # st.rerun()를 제거합니다.
