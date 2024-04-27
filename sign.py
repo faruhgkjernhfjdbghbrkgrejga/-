f3d0a91aee1d3303d99edc78c9f348cffdc43d6e
88nnn <130357067+88nnn@users.noreply.github.com>
2024-04-27 13:14:57 +0900

    Update sign.py

--- sign.py
+++ sign.py
@@ -1,10 +1,17 @@
 import streamlit as st
+import streamlit_authenticator as auth
+
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=auth.SafeLoader)
 
 # 샘플 사용자 데이터베이스 (실제로는 데이터베이스나 API 호출로 대체해야 합니다)
-users = {
-    "user1": {"password": "password1"},
-    "user2": {"password": "password2"}
-}
+authenticator = auth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
 
 def register_user(username, password):
     """
@@ -23,7 +30,7 @@ def register_user(username, password):
         users[username] = {"password": password}
         return f"'{username}' 사용자가 성공적으로 등록되었습니다."
 
-def login_user(username, password):
+def login_user(name, authentication_status, username, password):
     """
     기존 사용자를 로그인합니다.
 
@@ -34,10 +41,21 @@ def login_user(username, password):
     Returns:
         str: 성공적인 로그인 또는 오류 메시지.
     """
-    if username in users and users[username]["password"] == password:
+
+    name, authentication_status, username = authenticator.login("Login","main")
+    # authentication_status : 인증 상태 (실패=>False, 값없음=>None, 성공=>True)
+    if authentication_status == False:
+        st.error("잘못된 사용자 이름 또는 비밀번호입니다.")
+    if authentication_status == None:
+        st.warning("사용자 이름 또는 비밀번호를 입력해주세요.")
+    if authentication_status:
+        return f"'{username}' 사용자가 성공적으로 로그인되었습니다."
+    
+    """if username in users and users[username]["password"] == password:
         return f"'{username}' 사용자가 성공적으로 로그인되었습니다."
     else:
         return "오류: 잘못된 사용자 이름 또는 비밀번호입니다."
+        """
 
 def main():
     st.title("User Registration & Login")
