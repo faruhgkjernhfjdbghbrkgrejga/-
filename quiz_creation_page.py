853da30fa27529f7f7d55c74e0172ae2c6683dfc
faruhgkjernhfjdbghbrkgrejga <165423455+faruhgkjernhfjdbghbrkgrejga@users.noreply.github.com>
2024-05-11 07:11:21 +0900

    Update quiz_creation_page.py

--- quiz_creation_page.py
+++ quiz_creation_page.py
@@ -189,17 +189,16 @@ def quiz_creation_page(text_content):
                     document_chainsub = create_stuff_documents_chain(llm, promptsub)
                     document_chaintf = create_stuff_documents_chain(llm, prompttf)
 
-                    retriever = vector.as_retriever()
+                    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, output_parser=parseroub)
+                    retrieval_chainoub = qa
                     
-                    retrieval_chainoub = create_retrieval_chain(retriever, document_chainoub)
                     retrieval_chainsub = create_retrieval_chain(retriever, document_chainsub)
                     retrieval_chaintf = create_retrieval_chain(retriever, document_chaintf)
-                    
+
+                    quiz_query = "Create one multiple-choice question focusing on important concepts, following the given format, referring to the following context"
 
                     for i in range(num_quizzes):
-                        relevant_docs = qa.get_relevant_documents(
-                            "Create one multiple-choice question focusing on important concepts, following the given format, referring to the following context"
-                        )
+                        relevant_docs = qa.get_relevant_documents(quiz_query)
                         response = qa.run(relevant_docs)
                         quiz_questions.append(response)
                         progress = (i + 1) / num_quizzes
