7f4511dd792bb2802970c60fddce62e11634e9a2
faruhgkjernhfjdbghbrkgrejga <165423455+faruhgkjernhfjdbghbrkgrejga@users.noreply.github.com>
2024-05-11 08:07:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import streamlit as st
 from quiz_creation_page import quiz_creation_page
 from upload_page import upload_page
 from quiz_solve_page import quiz_solve_page
-from quiz_grading_page import quiz_creatquiz_grading_pageion_page
+from quiz_grading_page import quiz_grading_page
 from sign import sign
 
 # 페이지 타이틀 설정
