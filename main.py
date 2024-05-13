06400ee8627ac0eba5ef480572f326f1d3cc6078
faruhgkjernhfjdbghbrkgrejga <165423455+faruhgkjernhfjdbghbrkgrejga@users.noreply.github.com>
2024-05-14 04:24:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,15 +14,15 @@ def main():
 
     # 선택된 페이지 표시
     if selected_page == "파일 업로드":
-        upload_page.upload_page()
+        upload_page()
     elif selected_page == "퀴즈 생성":
         quiz_creation_page.quiz_creation_page()
     elif selected_page == "퀴즈 풀이":
-        quiz_solve_page.quiz_solve_page()
+        quiz_solve_page()
     elif selected_page == "퀴즈 채점":
-        quiz_grading_page.quiz_grading_page()
+        quiz_grading_page()
     elif selected_page == "로그인":
-        sign.sign()
+        sign()
 
 if __name__ == "__main__":
     main()
