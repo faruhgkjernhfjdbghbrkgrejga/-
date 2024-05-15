ecf2e6a9577a0b0e705b32421f7bad04ed9faa3e
faruhgkjernhfjdbghbrkgrejga <165423455+faruhgkjernhfjdbghbrkgrejga@users.noreply.github.com>
2024-05-16 02:15:05 +0900

    Update quiz_creation_page.py

--- pages/quiz_creation_page.py
+++ pages/quiz_creation_page.py
@@ -89,9 +89,9 @@ def process_file(uploaded_file, text_area_content, url_area_content):
     if uploaded_file is not None:
         if uploaded_file.type == "text/plain":
             text_content = uploaded_file.read().decode("utf-8")
-        elif uploaded_file.type.startswith("image/"):
-            image = Image.open(uploaded_file)
-            text_content = pytesseract.image_to_string(image)
+#        elif uploaded_file.type.startswith("image/"):
+#            image = Image.open(uploaded_file)
+#            text_content = pytesseract.image_to_string(image)
         elif uploaded_file.type == "application/pdf":
             pdf_reader = PdfReader(io.BytesIO(uploaded_file.read()))
             text_content = ""
