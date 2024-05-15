3a71313ee757f3c3a56a90e53c984aa8ce11c82b
faruhgkjernhfjdbghbrkgrejga <165423455+faruhgkjernhfjdbghbrkgrejga@users.noreply.github.com>
2024-05-15 22:53:54 +0900

    Update quiz_creation_page.py

--- pages/quiz_creation_page.py
+++ pages/quiz_creation_page.py
@@ -213,6 +213,7 @@ def quiz_creation_page():
 
             # 파일 업로드 옵션
             st.header("파일 업로드")
+            uploaded_file = process_file(uploaded_file, text_area_content)
             #uploaded_file = st.file_uploader("텍스트, 이미지, 또는 PDF 파일을 업로드하세요.", type=["txt", "jpg", "jpeg", "png", "pdf"])
 
             text_content = process_file(uploaded_file)
