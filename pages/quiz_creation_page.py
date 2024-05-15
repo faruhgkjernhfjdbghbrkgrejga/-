3f35fa3644193b16390aef46a08667bd3314f978
faruhgkjernhfjdbghbrkgrejga <165423455+faruhgkjernhfjdbghbrkgrejga@users.noreply.github.com>
2024-05-16 03:47:16 +0900

    Update quiz_creation_page.py

--- pages/quiz_creation_page.py
+++ pages/quiz_creation_page.py
@@ -166,6 +166,10 @@ def process_file(uploaded_file):
                     return None
 
     # 업로드된 파일 처리
+    if uploaded_file is None:
+        st.warning("파일을 업로드하세요.")
+        return None
+
     if uploaded_file.type.startswith("image/"):
         image = Image.open(uploaded_file)
         text_content = pytesseract.image_to_string(image)
@@ -174,13 +178,6 @@ def process_file(uploaded_file):
         text_content = ""
         for page in pdf_reader.pages:
             text_content += page.extract_text()
-    elif url_area_content.type == "text/plain":
-        url = url_area_content.read().decode("utf-8")
-        loader = RecursiveUrlLoader(url=url)
-        text_content = loader.load()
-    elif text_area_content.type == "text/plain":
-        text_content = text_area_content.read().decode("utf-8")
-        
     else:
         st.error("지원하지 않는 파일 형식입니다.")
         return None
