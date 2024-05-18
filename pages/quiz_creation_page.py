2b92e27373ead39c8c830639056b6fe6e31d0f02
faruhgkjernhfjdbghbrkgrejga <165423455+faruhgkjernhfjdbghbrkgrejga@users.noreply.github.com>
2024-05-19 05:41:58 +0900

    Update quiz_creation_page.py

--- pages/quiz_creation_page.py
+++ pages/quiz_creation_page.py
@@ -158,12 +158,12 @@ def process_file(uploaded_file):
     else:
         url_area_content = None
     
-    if uploaded_file is None:
-        if url_area_content is None:
-            if selected_topic == "토픽 선택":
-                if text_area_content is None:
-                    st.warning("입력이 필요합니다.")
-                    return None
+    # if uploaded_file is None:
+    #     if url_area_content is None:
+    #         if selected_topic == "토픽 선택":
+    #             if text_area_content is None:
+    #                 st.warning("입력이 필요합니다.")
+    #                 return None
 
     # 업로드된 파일 처리
     if uploaded_file is None:
