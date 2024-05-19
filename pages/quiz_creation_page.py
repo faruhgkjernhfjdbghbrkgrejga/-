c3e5f462dcb4863e36cefdd651728c071fb80238
4728 <165423455+faruhgkjernhfjdbghbrkgrejga@users.noreply.github.com>
2024-05-19 13:23:59 +0900

    Update quiz_creation_page.py

--- pages/quiz_creation_page.py
+++ pages/quiz_creation_page.py
@@ -82,41 +82,55 @@ def make_model(pages):
     # chaintf = prompttf | chat_model | parsertf
     return 0
 
-@st.cache_data
-def process_file(uploaded_file, text_area_content, url_area_content):
-    text_content = None
+# @st.cache_data
+# def process_file(uploaded_file, text_area_content, url_area_content):
+#     text_content = None
 
-    if uploaded_file is not None:
-        if uploaded_file.type == "text/plain":
-            text_content = uploaded_file.read().decode("utf-8")
-        elif uploaded_file.type.startswith("image/"):
-            image = Image.open(uploaded_file)
-            text_content = pytesseract.image_to_string(image)
-        elif uploaded_file.type == "application/pdf":
-            pdf_reader = PdfReader(io.BytesIO(uploaded_file.read()))
-            text_content = ""
-            for page in pdf_reader.pages:
-                text_content += page.extract_text()
-        else:
-            st.error("지원하지 않는 파일 형식입니다.")
-            return None
-    elif text_area_content:
-        text_content = text_area_content
-    elif url_area_content:
-        loader = RecursiveUrlLoader(url=url_area_content)
-        text_content = loader.load()
+#     if uploaded_file is not None:
+#         if uploaded_file.type == "text/plain":
+#             text_content = uploaded_file.read().decode("utf-8")
+#         elif uploaded_file.type.startswith("image/"):
+#             image = Image.open(uploaded_file)
+#             text_content = pytesseract.image_to_string(image)
+#         elif uploaded_file.type == "application/pdf":
+#             pdf_reader = PdfReader(io.BytesIO(uploaded_file.read()))
+#             text_content = ""
+#             for page in pdf_reader.pages:
+#                 text_content += page.extract_text()
+#         else:
+#             st.error("지원하지 않는 파일 형식입니다.")
+#             return None
+#     elif text_area_content:
+#         text_content = text_area_content
+#     elif url_area_content:
+#         loader = RecursiveUrlLoader(url=url_area_content)
+#         text_content = loader.load()
 
-    if text_content:
-        documents = [{"page_content": text_content}]
-        text_splitter = RecursiveCharacterTextSplitter()
-        documents = text_splitter.split_documents(documents)
-        return documents
-    else:
-        st.warning("파일, 텍스트 또는 URL을 입력하세요.")
-        return None
+#     if text_content:
+#         documents = [{"page_content": text_content}]
+#         text_splitter = RecursiveCharacterTextSplitter()
+#         documents = text_splitter.split_documents(documents)
+#         return documents
+#     else:
+#         st.warning("파일, 텍스트 또는 URL을 입력하세요.")
+#         return None
 
-    return text_content
+#     return text_content
 
+def process_text():
+    text_area_content = st.text_area("텍스트를 입력하세요.")
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        # Set a really small chunk size, just to show.
+        chunk_size=100,
+        chunk_overlap=20,
+        length_function=len,
+        is_separator_regex=False,
+    )
+    if text_area_content is not None:
+        text_content = process_file(uploaded_file, text_area_content)
+    texts = text_splitter.create_documents([text_content])
+    return texts
 
 # 파일 처리 함수
 def process_file(uploaded_file, upload_option):
@@ -137,33 +151,33 @@ def process_file(uploaded_file, upload_option):
     else:
         uploaded_file = None
 
-    # 텍스트 입력 영역
-    if upload_option == "직접 입력":
-        text_area_content = st.text_area("텍스트를 입력하세요.")
-    else:
-        text_area_content = None
+    # # 텍스트 입력 영역
+    # if upload_option == "직접 입력":
+    #     text_area_content = st.text_area("텍스트를 입력하세요.")
+    # else:
+    #     text_area_content = None
 
-    # URL 입력 영역
-    if upload_option == "URL":
-        url_area_content = st.text_area("URL을 입력하세요.")
-    else:
-        url_area_content = None
+    # # URL 입력 영역
+    # if upload_option == "URL":
+    #     url_area_content = st.text_area("URL을 입력하세요.")
+    # else:
+    #     url_area_content = None
 
-    # 토픽 선택 영역
-    if upload_option == "토픽 선택":
-        selected_topic = "수학"
-        selected_topic = st.selectbox(
-            "토픽을 선택하세요.",
-            ("토픽 선택", "수학", "물리학", "역사", "화학"))
-    else:
-        url_area_content = None
+    # # 토픽 선택 영역
+    # if upload_option == "토픽 선택":
+    #     selected_topic = "수학"
+    #     selected_topic = st.selectbox(
+    #         "토픽을 선택하세요.",
+    #         ("토픽 선택", "수학", "물리학", "역사", "화학"))
+    # else:
+    #     url_area_content = None
     
-    if uploaded_file is None:
-        if url_area_content is None:
-            if selected_topic == "토픽 선택":
-                if text_area_content is None:
-                    st.warning("입력이 필요합니다.")
-                    return None
+    # if uploaded_file is None:
+    #     if url_area_content is None:
+    #         if selected_topic == "토픽 선택":
+    #             if text_area_content is None:
+    #                 st.warning("입력이 필요합니다.")
+    #                 return None
 
     # 업로드된 파일 처리
     if uploaded_file is None:
@@ -253,7 +267,14 @@ def quiz_creation_page():
             st.header("파일 업로드")
             uploaded_file = None
             #uploaded_file = st.file_uploader("텍스트, 이미지, 또는 PDF 파일을 업로드하세요.", type=["txt", "jpg", "jpeg", "png", "pdf"])
-            text_content = process_file(uploaded_file, upload_option)
+
+            if upload_option == "직접 입력":
+                process_text()
+
+
+            else:
+                text_content = process_file(uploaded_file, upload_option)
+            
 
             quiz_questions = []
 
