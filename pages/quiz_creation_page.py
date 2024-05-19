d305d80f9a243d9654fadb42c32a86ae25900a2e
4728 <165423455+faruhgkjernhfjdbghbrkgrejga@users.noreply.github.com>
2024-05-19 13:07:35 +0900

    Update quiz_creation_page.py

--- pages/quiz_creation_page.py
+++ pages/quiz_creation_page.py
@@ -187,7 +187,9 @@ def process_file(uploaded_file, text_area_content, url_area_content):
     # if text_area_content is not None:
     #     text_content = process_file(uploaded_file, text_area_content)
     text_content = text_splitter.create_documents([text_content])
-    return text_content
+    return texts
+
+    return texts
 
 
 # 퀴즈 생성 함수
