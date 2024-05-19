f8dbaa67439eb6b162dc0a3fa12965848097b2ee
4728 <165423455+faruhgkjernhfjdbghbrkgrejga@users.noreply.github.com>
2024-05-19 12:27:31 +0900

    Update quiz_creation_page.py

--- pages/quiz_creation_page.py
+++ pages/quiz_creation_page.py
@@ -31,14 +31,14 @@ class CreateQuizoub(BaseModel):
     correct_answer: str = Field(description="One of the options1 or options2 or options3 or options4")
 
 class CreateQuizsub(BaseModel):
-    quiz: str = Field(description="The created problem")
-    correct_answer: str = Field(description="The answer to the problem")
+    quiz = ("quiz =The created problem")
+    correct_answer = ("correct_answer =The answer to the problem")
 
 class CreateQuizTF(BaseModel):
-    quiz: str = Field(description="The created problem")
-    options1: str = Field(description="The true or false option of the created problem")
-    options2: str = Field(description="The true or false option of the created problem")
-    correct_answer: str = Field(description="One of the options1 or options2")
+    quiz = ("The created problem")
+    options1 = ("The true or false option of the created problem")
+    options2 = ("The true or false option of the created problem")
+    correct_answer = ("One of the options1 or options2")
 
 def make_model(pages):
     llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
@@ -56,8 +56,12 @@ def make_model(pages):
 
     prompt = PromptTemplate.from_template(
         "Question: {input}, Please answer in KOREAN."
-        "CONTEXT: {context}."
-        "FORMAT: {format}"
+
+        "CONTEXT:"
+        "{context}."
+
+        "FORMAT:"
+        "{format}"
     )
     promptoub = prompt.partial(format=parseroub.get_format_instructions())
     promptsub = prompt.partial(format=parsersub.get_format_instructions())
@@ -73,7 +77,10 @@ def make_model(pages):
     retrieval_chainsub = create_retrieval_chain(retriever, document_chainsub)
     retrieval_chaintf = create_retrieval_chain(retriever, document_chaintf)
 
-    return retrieval_chainoub, retrieval_chainsub, retrieval_chaintf
+    # chainoub = promptoub | chat_model | parseroub
+    # chainsub = promptsub | chat_model | parsersub
+    # chaintf = prompttf | chat_model | parsertf
+    return 0
 
 @st.cache_data
 def process_file(uploaded_file, text_area_content, url_area_content):
@@ -101,34 +108,114 @@ def process_file(uploaded_file, text_area_content, url_area_content):
 
     if text_content:
         documents = [{"page_content": text_content}]
+        text_splitter = RecursiveCharacterTextSplitter()
+        documents = text_splitter.split_documents(documents)
         return documents
     else:
         st.warning("파일, 텍스트 또는 URL을 입력하세요.")
         return None
-        
+
     return text_content
 
+
+# 파일 처리 함수
+def process_file(uploaded_file):
+
+    uploaded_file = None
+    text_area_content = None
+    url_area_content = None
+    selected_topic = None
+    
+    # 파일 업로드 옵션 선택
+    upload_option = st.radio("입력 유형을 선택하세요", ("이미지 파일", "PDF 파일", "직접 입력", "URL", "토픽 선택"))
+
+    # 선택된 옵션에 따라 입력 방식 제공
+    if upload_option == "이미지 파일":
+        uploaded_file = st.file_uploader("이미지 파일을 업로드하세요.", type=["jpg", "jpeg", "png"])
+    elif upload_option == "PDF 파일":
+        uploaded_file = st.file_uploader("PDF 파일을 업로드하세요.", type=["pdf"])
+    else:
+        uploaded_file = None
+
+    # 텍스트 입력 영역
+    if upload_option == "직접 입력":
+        text_area_content = st.text_area("텍스트를 입력하세요.")
+    else:
+        text_area_content = None
+
+    # URL 입력 영역
+    if upload_option == "URL":
+        url_area_content = st.text_area("URL을 입력하세요.")
+    else:
+        url_area_content = None
+
+    # 토픽 선택 영역
+    if upload_option == "토픽 선택":
+        selected_topic = "수학"
+        selected_topic = st.selectbox(
+            "토픽을 선택하세요.",
+            ("토픽 선택", "수학", "물리학", "역사", "화학"))
+    else:
+        url_area_content = None
+    
+    if uploaded_file is None:
+        if url_area_content is None:
+            if selected_topic == "토픽 선택":
+                if text_area_content is None:
+                    st.warning("입력이 필요합니다.")
+                    return None
+
+    # 업로드된 파일 처리
+    if uploaded_file is None:
+        st.warning("파일을 업로드하세요.")
+        return None
+
+    if uploaded_file.type.startswith("image/"):
+        image = Image.open(uploaded_file)
+        text_content = pytesseract.image_to_string(image)
+    elif uploaded_file.type == "application/pdf":
+        pdf_reader = PdfReader(io.BytesIO(uploaded_file.read()))
+        text_content = ""
+        for page in pdf_reader.pages:
+            text_content += page.extract_text()
+    else:
+        st.error("지원하지 않는 파일 형식입니다.")
+        return None
+        
+    text_splitter = RecursiveCharacterTextSplitter(
+        # Set a really small chunk size, just to show.
+        chunk_size=100,
+        chunk_overlap=20,
+        length_function=len,
+        is_separator_regex=False,
+    )
+    if text_area_content is not None:
+        text_content = process_file(uploaded_file, text_area_content)
+    texts = text_splitter.create_documents([text_content])
+    return texts
+
+    return texts
+
+# 퀴즈 생성 함수
+@st.experimental_fragment
 def generate_quiz(quiz_type, text_content, retrieval_chainoub, retrieval_chainsub, retrieval_chaintf):
     # Generate quiz prompt based on selected quiz type
     if quiz_type == "다중 선택 (객관식)":
         response = retrieval_chainoub.invoke(
             {
-                "input": "Create one multiple-choice question focusing on important concepts, following the given format, referring to the following context",
-                "context": text_content
+                "input": "Create one multiple-choice question focusing on important concepts, following the given format, referring to the following context"
             }
         )
     elif quiz_type == "주관식":
         response = retrieval_chainsub.invoke(
             {
-                "input": "Create one open-ended question focusing on important concepts, following the given format, referring to the following context",
-                "context": text_content
+                "input": "Create one open-ended question focusing on important concepts, following the given format, referring to the following context"
             }
         )
     elif quiz_type == "OX 퀴즈":
         response = retrieval_chaintf.invoke(
             {
-                "input": "Create one true or false question focusing on important concepts, following the given format, referring to the following context",
-                "context": text_content
+                "input": "Create one true or false question focusing on important concepts, following the given format, referring to the following context"
             }
         )
     quiz_questions = response
@@ -145,18 +232,14 @@ def grade_quiz_answer(user_answer, quiz_answer):
 
 # 메인 함수
 def quiz_creation_page():
-    # Initialize session state
-    if 'selected_page' not in st.session_state:
-        st.session_state.selected_page = ""
-    if 'quiz_created' not in st.session_state:
-        st.session_state.quiz_created = False
-
     placeholder = st.empty()
     st.session_state.page = 0
     if st.session_state.page == 0:
         with placeholder.container():
             st.title("AI 퀴즈 생성기")
-            
+            if 'selected_page' not in st.session_state:
+                st.session_state.selected_page = ""
+
             # 퀴즈 유형 선택
             quiz_type = st.radio("생성할 퀴즈 유형을 선택하세요:", ["다중 선택 (객관식)", "주관식", "OX 퀴즈"])
 
@@ -165,18 +248,13 @@ def quiz_creation_page():
 
             # 파일 업로드 옵션
             st.header("파일 업로드")
-            uploaded_file = st.file_uploader("텍스트, 이미지, 또는 PDF 파일을 업로드하세요.", type=["txt", "jpg", "jpeg", "png", "pdf"])
-            text_area_content = st.text_area("텍스트를 입력하세요.")
-            url_area_content = st.text_area("URL을 입력하세요.")
+            uploaded_file = None
+            #uploaded_file = st.file_uploader("텍스트, 이미지, 또는 PDF 파일을 업로드하세요.", type=["txt", "jpg", "jpeg", "png", "pdf"])
+            text_content = process_file(uploaded_file)
 
-            text_content = process_file(uploaded_file, text_area_content, url_area_content)
+            quiz_questions = []
 
             if text_content is not None:
-                st.write("Text content processed successfully:")
-                st.write(text_content)
-                st.write("Creating document structure for text splitting:")
-                documents = [{"page_content": text_content}]
-                st.write(documents)
 
                 if st.button('문제 생성 하기'):
                     with st.spinner('퀴즈를 생성 중입니다...'):
@@ -185,7 +263,7 @@ def quiz_creation_page():
 
                         # Rag
                         text_splitter = RecursiveCharacterTextSplitter()
-                        #documents = text_splitter.split_documents(text_content)
+                        documents = text_splitter.split_documents(text_content)
                         vector = FAISS.from_documents(documents, embeddings)
 
                         # PydanticOutputParser 생성
@@ -230,6 +308,7 @@ def quiz_creation_page():
                 if st.session_state.get('quiz_created', False):
                     if st.button('퀴즈 풀기'):
                         st.switch_page("pages/quiz_solve_page.py")
+
 
 if __name__ == "__main__":
     quiz_creation_page()
