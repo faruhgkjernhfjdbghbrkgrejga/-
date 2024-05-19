d048c96da858021da48863509d480e83baea8447
4728 <165423455+faruhgkjernhfjdbghbrkgrejga@users.noreply.github.com>
2024-05-19 13:33:52 +0900

    Update quiz_creation_page.py

--- pages/quiz_creation_page.py
+++ pages/quiz_creation_page.py
@@ -259,7 +259,12 @@ def quiz_creation_page():
             #uploaded_file = st.file_uploader("텍스트, 이미지, 또는 PDF 파일을 업로드하세요.", type=["txt", "jpg", "jpeg", "png", "pdf"])
 
             if upload_option == "직접 입력":
-                text_content = st.text_area("텍스트를 입력하세요.")
+                documents = st.text_area("텍스트를 입력하세요.")
+
+            elif upload_option == "URL":
+                url_area_content = st.text_area("URL을 입력하세요.")
+                loader = RecursiveUrlLoader(url=url_area_content)
+                text_content = loader.load()
                 
             else:
                 text_content = process_file(uploaded_file, upload_option)
