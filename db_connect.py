d9c6b6a5dfeaa79cbaed9c966f990112cee3f4bb
88nnn <130357067+88nnn@users.noreply.github.com>
2024-05-19 14:01:24 +0900

    Update db_connect.py

--- db_connect.py
+++ db_connect.py
@@ -12,9 +12,8 @@ from langchain_core.pydantic_v1 import BaseModel, Field
 class QuizInput(BaseModel):
     input_text: str = Field(description="사용자 입력 텍스트")
 
-# Define function to process user input and retrieve results using RAG model
 def retrieve_results(user_query):
-    # Create MongoDB Atlas Vector Search
+    # MongoDB Atlas Vector Search 생성
     vector_search = MongoDBAtlasVectorSearch.from_connection_string(
         "mongodb+srv://acm41th:vCcYRo8b4hsWJkUj@cluster0.ctxcrvl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
         "sample_mflix.embedded_movies",
@@ -22,21 +21,25 @@ def retrieve_results(user_query):
         index_name="vector_index"
     )
 
-    # Define prompt template
+    # 사용자 입력에 대한 프롬프트 템플릿 정의
     prompt_template = PromptTemplate.from_template(
-        "{input}, Please answer in KOREAN."
-        "CONTEXT:"
+        "{input}, Answer please."
+        "context:"
         "{context}."
-        "FORMAT:"
+        "topic:"
+        "{topic}."
+        "characteristics:"
+        "{characteristics}."
+        "format:"
         "{format}"
     )
 
-    # Parse input format
-    parser = PydanticOutputParser(pydantic_object=QuizInput)
-    prompt = prompt_template.partial(format=parser.get_format_instructions())
+    # 프롬프트 생성
+    prompt = prompt_template.partial(format="퀴즈 답변 형식을 지정하세요")
 
-    # Generate prompt with user input
-    response = results = vector_search.similarity_search_with_score(
-    query=query, k=5, pre_filter={"page": {"$eq": 1}})
+    # 사용자 입력과 (벡터 유사도)를 고려하여 검색
+    #response = vector_search.invoke({"input": prompt.format(input=user_query)})
+    response = vector_search.similarity_search_with_score(
+    input=user_query, k=5, pre_filter={"page": {"$eq": 1}})
 
     return response
