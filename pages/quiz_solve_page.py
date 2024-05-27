59c700f42c756df0f183c6be55cf216e70748322
4728 <165423455+faruhgkjernhfjdbghbrkgrejga@users.noreply.github.com>
2024-05-28 05:05:02 +0900

    Update quiz_solve_page.py

--- pages/quiz_solve_page.py
+++ pages/quiz_solve_page.py
@@ -147,8 +147,8 @@ def quiz_solve_page():
 
         j += 1
     if st.session_state.number == st.session_state.selected_num:
+        st.session_state['total_score'] = sum(st.session_state.correct_answers)  # 정답 개수를 점수로 저장
         if st.button('퀴즈 채점'):
-            st.session_state['total_score'] = sum(st.session_state.correct_answers)  # 정답 개수를 점수로 저장
             st.switch_page("pages/quiz_grading_page.py")
 
     # 점수 확인 버튼 추가
