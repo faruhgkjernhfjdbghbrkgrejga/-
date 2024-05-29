1e3b92e84cd2d386e9d4eb706b218429034f9be8
4728 <165423455+faruhgkjernhfjdbghbrkgrejga@users.noreply.github.com>
2024-05-30 04:01:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import streamlit as st
-import quiz_creation_page
-import quiz_solve_page
-import quiz_grading_page
+import pages.quiz_creation_page
+import pages.quiz_solve_page
+import pages.quiz_grading_page
 import sign
+from streamlit_option_menu import option_menu
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 def main():
     st.sidebar.title("Navigation")
