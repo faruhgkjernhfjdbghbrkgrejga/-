bf9412a049433953f3491afe05d5e52fa7b74ecb
88nnn <130357067+88nnn@users.noreply.github.com>
2024-06-06 11:21:30 +0900

    Update sign.py

--- pages/sign.py
+++ pages/sign.py
@@ -1,102 +1,49 @@
-#sign.py
+#sign
 
-import yaml
 import streamlit as st
-import bcrypt
-import hashlib
+import boto3
 
-
-def register_user(name, username, email, password):
-    """
-    새로운 사용자를 등록합니다.
-
-    Args:
-        username (str): 원하는 사용자 이름.
-        password (str): 원하는 비밀번호.
-
-    Returns:
-        str: 성공적인 등록 또는 오류 메시지.
-    """
-    # 비밀번호 해싱
-    hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
-
-    # 기존 데이터 읽기
-    with open('config.yaml', 'r') as file:
-        existing_data = yaml.safe_load(file)
+def register_cognito_user(username, password, email):
+    region_name = 'us-east-1'
+    client_id = '7bdv436rrb0l7nhbsva60t7242'
+    user_pool_id = 'us-east-1_pJbggBo44'
     
-    # 새로운 계정 정보 추가
-    if username in existing_data['credentials']['usernames']:
-        st.error("이미 사용 중인 사용자 이름입니다.")
-        return "계정 생성 실패: 이미 사용 중인 사용자 이름입니다."
-    else:
-        new_data = {
-            "credentials": {
-                "usernames": {
-                    username: {
-                        "email": email,
-                        "name": name,
-                        "password": hashed_password
-                    }
-                }
-            }
-        }
+    cognito_client = boto3.client('cognito-idp', region_name=region_name)
 
-        existing_data['credentials']['usernames'].update(new_data['credentials']['usernames'])
-        
-        # YAML 파일에 쓰기
-        with open('config.yaml', 'w') as file:
-            yaml.dump(existing_data, file, default_flow_style=False)
-        
-        st.success("계정이 성공적으로 생성되었습니다!")
-        return "계정이 성공적으로 생성되었습니다!"
-
-
-
-def login_user(username, password):
-    """
-    기존 사용자를 로그인합니다.
-
-    Args:
-        username (str): 사용자 이름.
-        password (str): 비밀번호.
-
-    Returns:
-        str: 성공적인 로그인 또는 오류 메시지.
-    """
-    # 사용자 정보 확인 및 인증
-    with open('config.yaml', 'r') as file:
-        existing_data = yaml.safe_load(file)
-    
-    user_info = existing_data['credentials']['usernames'].get(username)
-    if user_info is None:
-        st.error("잘못된 사용자 이름 또는 비밀번호입니다.")
-        return
-    if bcrypt.checkpw(password.encode(), user_info["password"].encode()):
-        return f"'{username}' 사용자가 성공적으로 로그인되었습니다."
-    else:
-        st.error("잘못된 사용자 이름 또는 비밀번호입니다.")
+    try:
+        response = cognito_client.sign_up(
+            ClientId=client_id,
+            Username=username,
+            Password=password,
+            UserAttributes=[
+                {
+                    'Name': 'email',
+                    'Value': email
+                },
+            ]
+        )
+        return True
+    except cognito_client.exceptions.UsernameExistsException:
+        st.error("회원가입 실패: 이미 존재하는 사용자 이름입니다.")
+        return False
+    except Exception as e:
+        st.error(f"회원가입 중 오류 발생: {str(e)}")
+        return False
 
 def sign():
-    st.title("User Registration & Login")
-    
-    # User registration
-    st.header("Register")
-    new_name = st.text_input("Enter your name:")
-    new_username = st.text_input("Enter a new username:")
-    new_email = st.text_input("Enter your email:")
-    new_password = st.text_input("Enter a new password:", type="password")
-    if st.button("Register"):
-        result = register_user(new_name, new_username, new_email, new_password)
-        st.success(result)
-        
-    # User login
-    st.header("Login")
-    existing_username = st.text_input("Enter your username:", key="username_input")
-    existing_password = st.text_input("Enter your password:", type="password", key="password_input")
-    if st.button("Login", key="login_button"):
-        result = login_user(existing_username, existing_password)
-        if result:
-            st.success(result)
+    st.title("회원가입")
+
+    new_username = st.text_input("새 사용자 이름을 입력하세요:")
+    new_password = st.text_input("새 비밀번호를 입력하세요:", type="password")
+    new_email = st.text_input("이메일 주소를 입력하세요:")
+
+    if st.button("회원가입"):
+        if register_cognito_user(new_username, new_password, new_email):
+            st.success("회원가입이 완료되었습니다.")
+            st.write("운영자가 가입을 승인할 때까지 기다려주세요!")
+            st.experimental_rerun()
+        else:
+            st.error("회원가입에 실패했습니다. 다시 시도하세요.")
 
 if __name__ == "__main__":
     sign()
